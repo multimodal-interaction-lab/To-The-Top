0c8cbd619fa45f9c1fe197c0b937360aab6f8a03
6henrykim <22160472+6henrykim@users.noreply.github.com>
2021-05-21 11:33:13 -0500

    Made DespawnPlane trigger BuildingBlock.Despawn()

---
+++ To The Top/Assets/Scripts/Boundary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Detected");
+
+        if (other.gameObject.TryGetComponent(typeof(BuildingBlock), out Component component))
+        {
+            ((BuildingBlock)component).Despawn() ;
+        }
+    }
+}
