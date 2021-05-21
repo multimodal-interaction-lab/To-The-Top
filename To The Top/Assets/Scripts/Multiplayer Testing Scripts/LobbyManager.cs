51381d89a7a25236114a28627b68f7cf24a0a624
Kakoure <31775189+Kakoure@users.noreply.github.com>
2021-05-21 14:54:15 -0500

    Setting up
    Currently has an issue where instantiating the leap motion rig does not render the hands, although it appears to be tracking

---
+++ To The Top/Assets/Scripts/Multiplayer Testing Scripts/LobbyManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LobbyManager : MonoBehaviour
+{
+
+    string gameVersion = "1";
+    [SerializeField]
+    GameObject buttonPanel;
+    [SerializeField]
+    Text ConnectionStatusText;
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //buttonPanel.SetActive(false);
+        Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ConnectionStatusText.text = "Connection Status: "+ PhotonNetwork.NetworkClientState;
+    }
+
+    void Connect()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    public void OnConnectedToMaster()
+    {
+        buttonPanel.SetActive(true);
+    }
+
+    public void JoinGamePressed()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public void CreateGamePressed()
+    {
+        
+        if (PhotonNetwork.IsConnected)
+        {
+            string roomName = "Room " + Random.Range(1000, 10000);
+            RoomOptions options = new RoomOptions { MaxPlayers = 2, PlayerTtl = 10000 };
+            PhotonNetwork.CreateRoom(roomName, options, null);
+
+            PhotonNetwork.LoadLevel("MultiplayerTest-GameScene");
+        }
+
+        
+    }
+}
