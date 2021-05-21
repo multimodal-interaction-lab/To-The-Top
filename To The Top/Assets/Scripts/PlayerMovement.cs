6c68e701758c3d985bacce4411cdcfae0d7d4e0e
6henrykim <22160472+6henrykim@users.noreply.github.com>
2021-05-20 21:41:14 -0500

    Added script for player movement

--- To The Top/Assets/Scripts/PlayerMovement.cs
+++ To The Top/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,79 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float rotationSpeed;
+    public GameObject table;
+    public float rotationSpeed = 25f;
+    public float moveSpeed = 0.75f;
+
+    bool rotateLeftFlag;
+    bool rotateRightFlag;
+    bool moveUpFlag;
+    bool moveDownFlag;
 
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rotateLeftFlag = false;
+        rotateRightFlag = false;
+        moveUpFlag = false;
+        moveDownFlag = false;
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        
+        Debug.Log("rotateLeftFlag: " + rotateLeftFlag);
+        Debug.Log("rotateRightFlag: " + rotateRightFlag);
+        Debug.Log("moveUpFlag: " + moveUpFlag);
+        Debug.Log("moveDownFlag: " + moveDownFlag);
+        Debug.Log("position: " + transform.position);
+
+        if (rotateLeftFlag)
+        {
+            transform.RotateAround(table.transform.position, Vector3.up, rotationSpeed * Time.deltaTime * -1f);
+        }
+
+        if (rotateRightFlag)
+        {
+            transform.RotateAround(table.transform.position, Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+        if (moveUpFlag)
+        {
+            Vector3 moveVector = new Vector3(0f, moveSpeed * Time.deltaTime, 0f);
+            transform.position += moveVector;
+        }
+
+        if (moveDownFlag)
+        {
+            Vector3 moveVector = new Vector3(0f, moveSpeed * Time.deltaTime * -1f, 0f);
+            transform.position += moveVector;
+        }
+
+       
+
+
+
     }
+
+
+    // Functions called when button is pressed or unpressed
+    // Flags start out false, turn true when button is pressed
+    // and turn back to false when button is unpressed
+    public void FlipRotateLeftFlag()
+    {
+        rotateLeftFlag = !rotateLeftFlag;
+    }
+    public void FlipRotateRightFlag()
+    {
+        rotateRightFlag = !rotateRightFlag;
+    }
+    public void FlipMoveUpFlag()
+    {
+        moveUpFlag = !moveUpFlag;
+    }
+    public void FlipMoveDownFlag()
+    {
+        moveDownFlag = !moveDownFlag;
+    }
+
 }
